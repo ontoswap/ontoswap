4b30d6abbefded1f0c7b4004550681312461c190
ericswap <72806877+ericswap@users.noreply.github.com>
2020-10-14 10:09:23 +0800

    add contracts (#1)

---
+++ contracts/YFOToken.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.6.12;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract YFOToken is ERC20("YFOToken", "YFO"), Ownable {
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
+}
